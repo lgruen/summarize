04688989665017344b488f3a53a7de2c1bc3388b
Leonhard Gruenschloss <leonhard@gruenschloss.org>
2025-03-15 17:43:13 +1100

    Remove local deployment files replaced by GitHub Actions

--- redeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-gcloud builds submit --project leo-summarize --tag asia-southeast1-docker.pkg.dev/leo-summarize/images/summarize:latest
-gcloud --quiet run services delete --project leo-summarize --region asia-southeast1 summarize
-gcloud run services replace service.yaml --project leo-summarize --region asia-southeast1
-gcloud --project=leo-summarize run services add-iam-policy-binding --region=asia-southeast1 summarize --member="allUsers" --role="roles/run.invoker"
